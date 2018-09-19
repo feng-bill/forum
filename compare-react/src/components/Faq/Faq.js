9160b00c090a40fbeb054f7520799f3a9b812b88
Bill Feng <BingBao@Bills-MacBook-Pro.local>
2018-09-18 19:18:20 -0700

    new faq page

--- compare-react/src/components/Faq/Faq.js
+++ compare-react/src/components/Faq/Faq.js
@@ -3,57 +3,62 @@ import './Faq.css'
 import axios from 'axios';
 
 class Faq extends Component {
-
-    constructor(props) {
-        super(props)
-
-        //*define this.state equals a object.
-        //   pass in an array
-        this.state = {
-            faqs: []
-        };
-    }
-    
-    //React sets a react lifecycle by componentDidMount()
-    //when component loads, execute what ever inside
-    componentDidMount() {
-        axios.get('http://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                const faqs = res.data.slice(0,10);
-                this.setState({faqs});
-            })
-    }
-
-
     render() {
-        return (
+        return(
             <div>
-                <div className="container">
-                    <section className="section">
-                        <h1 className="title">FAQ</h1>
-                        <h2 className="subtitle is-4">Lorum ipsum</h2>
-
-                    <div className="columns">
-                        {/*In react, interate through a interable.
-                        all of these will be repeated based on this faqs property 
-                        will be in json */}
-                        {this.state.faqs.map(faq =>
-                            <div className="column is-one-third">
-                                <div className="card">
-                                <div className="card-content">
-                                    <p className="title">{ faq.title}</p>
-                                    <p className="answer">{ faq.body}</p>
-                                </div>
+                <nav class="navbar is-white">
+                    <div class="container">
+                        <div class="navbar-menu">
+                            <div class="navbar-start">
+                                <a class="navbar-item is-active" href="#">Popular</a>
+                                <a class="navbar-item" href="#">Recent</a>
+                                <a class="navbar-item" href="#">Rising</a>
+                            </div>
+                            <div class="navbar-end">
+                                <div class="navbar-item">
+                                    <input class="input" type="search" placeholder="Search forum..."></input>
                                 </div>
                             </div>
-                        )}
+                        </div>
+                    </div>
+		        </nav>
+                
+                <section className="container">
+                    <div className="columns">
+                        <div className="column is-9">
+                            <div className="box content">
+                                <article className="post">
+                                    <h4>Bill: I write stuff</h4>
+                                    <div className="media">
+                                        <div className="media-left">
+                                            <p><i className="fa fa-twitter"></i></p>
+                                        </div>
+                                        <div className="media-content">
+                                            <div className="content">
+                                                <p>
+                                                    <a href="#">@jsmith</a> replied 34 minutes ago &nbsp;
+                                                    <span class="tag">Question</span>
+                                                </p>
+                                            </div>
+                                        </div>  
+                                        <div className="media-right">
+                                            <span className="has-text-gray">
+                                                <i class="fa fa-comments"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </article> 
+                            </div>
+                        </div>
+                        <div className="column is-3">
+                            <a class="button is-primary is-block is-alt is-large" href="#">New Post</a>
+                        </div>
                     </div>
                 </section>
+
             </div>
-         </div>
-        );
-    }   
-    
+        )
+    };
 }
 
 export default Faq;
