2e249e191ed7ffb47177110e14cc1ae5524e8835
bill feng <bill21223@gmail.com>
2018-12-07 18:33:28 -0800

    ready to deploy again

--- v1.0/server.js
+++ v1.0/server.js
@@ -39,17 +39,16 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 app.use("/api/filter", filter);
 
-//Server static assets if in production
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  //Set Static folder
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  // Set static folder
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("running"));
+app.listen(port, () => console.log(`Server running on port ${port}`));
